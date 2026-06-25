e848d58a12a5476fee09eae53e5fe3f6adef4b59
Luis Corvalan <LRCorvalan@gmail.com>
2026-06-25 11:45:32 -0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('click', (event) => {
-  const link = event.target.closest('a[href="#app-store"]');
-  if (!link) return;
-  event.preventDefault();
-  alert('Add your App Store link here.');
-});
